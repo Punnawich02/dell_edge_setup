030611e9144fc2096f0700bfbbda52b8f26225ae
Punnawich02 <punnawich_dachin@cmu.ac.th>
2025-06-06 13:17:26 +0700

    fix: improve warning messages for reboot instructions

--- update.sh
+++ update.sh
@@ -80,8 +80,8 @@ while true; do
   if [ "${SHOULDREBOOT}" == "y" ]; then
     echo "reboot now"
     sleep 2
-    warning "Please run \"screen\" to use screen mode"
-    warning "And after reboot, please run \"sudo do-release-upgrade\" to upgrade to 20.04 LTS"
+    warning "After reboot, please run \"screen\" to use screen mode"
+    warning "and run \"sudo do-release-upgrade\" to upgrade to 20.04 LTS"
     sudo reboot
     break
   elif [ "${SHOULDREBOOT}" == "n" ]; then
